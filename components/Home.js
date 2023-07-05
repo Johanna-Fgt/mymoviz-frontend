da595edf89c8956a2657cd35d58fc416a6885017
johanna <johannafaget@gmail.com>
2023-07-05 12:03:12 +0200

    url

--- components/Home.js
+++ components/Home.js
@@ -61,7 +61,7 @@ function Home() {
 	});
 	useEffect(() => {
 		console.log('mount');
-		const URL = 'https://mymoviz-backend-two-kohl.vercel.app/movies';
+		const URL = 'https://mymoviz-backend-liart.vercel.app/movies';
 		fetch(URL)
 			.then((res) => res.json())
 			.then((d) => {
